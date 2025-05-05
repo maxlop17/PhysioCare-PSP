5cb686fb441e28777accda853e90ebeb322b0917
Maxim <maximlopezlopez@gmail.com>
2025-05-05 17:46:26 +0200

    Physio Profile View

---
+++ EmailProjectJavaFX/src/main/java/org/example/emailprojectjavafx/PhysioProfileViewController.java
@@ -0,0 +1,11 @@
+package org.example.emailprojectjavafx;
+
+import javafx.event.ActionEvent;
+
+public class PhysioProfileViewController {
+    public void onVerifyAppointments(ActionEvent actionEvent) {
+    }
+
+    public void onBackButtonClick(ActionEvent actionEvent) {
+    }
+}
